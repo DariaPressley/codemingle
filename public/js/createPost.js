e878eed876076649dabb49083ea56eb8996c4448
DariaPressley <daria.mik@gmail.com>
2023-10-24 13:12:54 -0700

    adding fron js for creating a post

---
+++ public/js/createPost.js
@@ -0,0 +1,36 @@
+async function newPostHandler(event) {
+    event.preventDefault();
+  
+    const code_block = document.querySelector('.file').value;
+    const text = document.querySelector('.textarea').value;
+    const user_name = document.querySelector('#user').value;
+  
+    const response = await fetch(`/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        code_block,
+        text,
+        user_name,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    if (response.ok) {
+      document.location.push('/');
+    } else {
+      alert('Failed to add post');
+    }
+  }
+  
+  document
+    .querySelector('.new-dish-form')
+    .addEventListener('submit', newPostHandler);
+
+
+var cancelButton = document.getElementById('cancelButton');
+    cancelButton.addEventListener('click', function() {
+    window.location.href = 'home.html'; 
+    });
+  
