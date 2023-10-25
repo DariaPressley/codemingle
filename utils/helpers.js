d7487d6ad92b74bbbfa5b5af37a2e5e3e0a60bde
afong <adam.y.w.fong@gmail.com>
2023-10-25 13:39:28 -0700

    buttons should now work for all posts instead of just first one

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,7 +4,6 @@ module.exports = {
       return date.toLocaleDateString();
     },
     check_id: (id1, id2) => {
-      console.log(id1 + ' space ' + id2);
       if (id1 === id2){
         return '<button class="delete"></button>';
       }
