a8d6c257c382410fd0ac5df1df8edbd05c01d625
DariaPressley <daria.mik@gmail.com>
2023-10-26 09:52:19 -0700

    added a delete button for user posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -31,9 +31,9 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(postData);
+    res.status(200).json(newPost);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message);
   }
 });
 
--- utils/helpers.js
+++ utils/helpers.js
@@ -5,7 +5,12 @@ module.exports = {
     },
     check_id: (id1, id2) => {
       if (id1 === id2){
-        return '<button class="delete"></button>';
+        return '<button class="delete"></button>'; 
+      }
+    },
+    delete_post: function (id1, id2, options) {
+      if (id1 === id2) {
+        return options.fn(this); 
       }
     }
   };
